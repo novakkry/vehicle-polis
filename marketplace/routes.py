a54d7a83bcdb1e53cefcacca73ffc55556885323
novakkry <novakkry@gmail.com>
2020-06-12 20:23:51 +1000

    base.html created, register form created, edited home.html so it derives from base.html, registration in progress

--- marketplace/routes.py
+++ marketplace/routes.py
@@ -1,11 +1,21 @@
-from flask import render_template, url_for, flash, redirect
+from flask import render_template, url_for, flash, redirect, flash, redirect
 from marketplace import app
+from marketplace.forms import RegistrationForm, LoginForm
 
 @app.route("/")
-@app.route("/home")
+@app.route("/home", methods=['GET','POST'])
 def home():
-    return render_template('home.html')
+    form = LoginForm()
+    return render_template('home.html', form=form)
 
 @app.route("/add_edit_item")
 def add_edit_item():
     return render_template('add_edit_item.html')
+
+@app.route("/register", methods=['GET','POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account successfuly created for {form.username.data}.', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
