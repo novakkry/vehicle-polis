6bea6a63b91515dfb558ed189c8f1df07cb1d194
novakkry <novakkry@gmail.com>
2020-06-18 23:18:00 +1000

    Final version

---
+++ README.txt
@@ -0,0 +1,3 @@
+Krystof Novak n10731067
+Kyle Holmes-Brown n10211756
+Jacob Knights n10604596
--- marketplace/config.py
+++ marketplace/config.py
@@ -1,3 +1,3 @@
 class Config:
     SECRET_KEY = 'thisissecret'
-    # SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
